0d2876f042cff5d20acf51013720c423f1d029c5
ccumaco <carloscumaco5@gmail.com>
2021-09-27 00:59:10 -0500

    list of objects notes

--- src/routes/notes.js
+++ src/routes/notes.js
@@ -1,8 +1,36 @@
 const express = require('express')
 const router = express.Router()
 
+const Note = require('../models/Note')
 
-router.get('/notes', (req,res) => {
-    res.send('notas desde la base de datos...')
+router.get('/notes/add', (req,res) => {
+    res.render('notes/new-note')
+})
+
+router.post('/notes/new-note', async (req,res) => {
+    const { title , description} = req.body
+    const errors = []
+    if (!title) {
+        errors.push({text: 'inserte un titulo'})
+    }
+    if (!description) {
+        errors.push({text: 'inserte una descripcion'})
+    }
+    if (errors.length > 0) {
+        res.render('notes/new-note', {
+            errors,
+            title,
+            description
+        })
+    } else {
+        const newNote = new Note({title, description})
+        await newNote.save();
+        res.redirect('/notes')
+    }
+})
+
+router.get('/notes', async (req, res) => {
+    const notes = await Note.find().lean()
+    res.render('notes/all-notes', { notes })
 })
 module.exports = router
