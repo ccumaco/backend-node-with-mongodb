c8262487ddb6d8682145f0088bfbe027ba61b62e
ccumaco <carloscumaco5@gmail.com>
2021-10-15 23:57:35 -0500

    end points products and categories

--- src/database.js
+++ src/database.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-mongoose.connect('mongodb://localhost/test')
+mongoose.connect('mongodb://localhost:27017/test')
 
     .then(db => console.log('DB is coneccted'))
     .catch(err => console.log(err, 'error'))
