581a4e8200da35f2195d74c8b8f7f419899ca19c
ccumaco <carloscumaco5@gmail.com>
2022-03-01 22:05:40 -0500

    end points commerce

--- src/routes/commerce.js
+++ src/routes/commerce.js
@@ -1,11 +1,29 @@
 const express = require('express')
 const router = express.Router()
 const Commerce = require('../models/Commerce')
-const { route } = require('./products')
 
 
 // crear comercio
-
+router.post('/commerce/new-commerce', async (req, res) => {
+    let { image , description, active,name,categories,promotion} = req.body
+    const commerceExist = await Commerce.find({name: name}).lean()
+    const errors = []
+    try{
+        if(commerceExist.length > 0) errors.push({alert: 'Ya existe un comercio con ese nombre'})
+    
+        if (!description) errors.push({alert: 'falta llenar la descripcion'})
+    
+        if (errors.length > 0) {
+            res.send(errors)
+        } else {
+            const newCommerce = new Commerce({image , description, active,name,categories,promotion})
+            await newCommerce.save();
+            res.send('se creo el comercio con exito')
+        }
+    } catch(error){
+        console.log(error);
+    }
+})
 
 //obtener comercio
 router.get('/commerce/:id', async (req, res) => {
@@ -23,7 +41,6 @@ router.get('/commerce/:id', async (req, res) => {
 router.get('/commerces', async (req, res) => {
     try {
         const commerce = await Commerce.find().lean()
-        console.log('los comercios son');
         res.send(commerce)
     } catch (error) {
         res.send('ocurrio algo inesperado')
@@ -37,7 +54,7 @@ router.delete('/commerce-delete/:id', async (req, res) => {
         const commerce = await Commerce.findByIdAndDelete(id)
         res.send(commerce)
     } catch (error) {
-        res.send('hay un error con el parametro enviado')
+        res.send('hay un error con el parametro enviado' + error)
     }
 })
 
