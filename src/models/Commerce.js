671f97463e009b0b8f94b689db5117b965825804
ccumaco <carloscumaco5@gmail.com>
2022-05-29 19:58:06 -0500

    model

--- src/models/Commerce.js
+++ src/models/Commerce.js
@@ -5,9 +5,7 @@ const CommerceSchema = new Schema({
     active: {type: Boolean, required: true},
     description: {type: String, required: true},
     image: {type: String, required: true},
-    name: {type: String, required: true},
-    categories: {type: Array, required: false},
-    promotion: {type: Number, required: false}
+    name: {type: String, required: true}
 })
 
 module.exports = mongoose.model('Commerce', CommerceSchema)
